d2ca9bff43fe1113bd4d0b6304b1570876ab9677
Josh Matthews <josh@jmatthews.us>
2014-01-17 23:02:46 -0500

    Changing layout names in uslesstile so it uses existing icons.

--- layout/uselesstile.lua
+++ layout/uselesstile.lua
@@ -196,13 +196,13 @@ local function tile(param, orientation)
 end
 
 right = {}
-right.name = "uselesstile"
+right.name = "tile"
 right.arrange = tile
 
 --- The main tile algo, on left.
 -- @param screen The screen number to tile.
 left = {}
-left.name = "uselesstileleft"
+left.name = "tileleft"
 function left.arrange(p)
     return tile(p, "left")
 end
@@ -210,7 +210,7 @@ end
 --- The main tile algo, on bottom.
 -- @param screen The screen number to tile.
 bottom = {}
-bottom.name = "uselesstilebottom"
+bottom.name = "tilebottom"
 function bottom.arrange(p)
     return tile(p, "bottom")
 end
@@ -218,7 +218,7 @@ end
 --- The main tile algo, on top.
 -- @param screen The screen number to tile.
 top = {}
-top.name = "uselesstiletop"
+top.name = "tiletop"
 function top.arrange(p)
     return tile(p, "top")
 end
